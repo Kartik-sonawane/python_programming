3272128ad4b89a96857374c903c78ac37859570a
Kartik Sonawane <80896718+Kartik-sonawane@users.noreply.github.com>
2021-03-31 12:37:30 +0530

    Update reverse_no.py

--- reverse_no.py
+++ reverse_no.py
@@ -2,11 +2,13 @@ number = int(input("Enter the number: "))
 rev_number = 0
 sum = 0
 
+#main logic
 while (number > 0):
     remainder = number % 10
     rev_number = (rev_number * 10) + remainder
-    sum = sum + remainder
+    sum = sum + remainder                        # sum of digits
     number = number // 10
 
+#printing 
 print("The reverse number is :",rev_number)
 print("the sum is :", sum)
